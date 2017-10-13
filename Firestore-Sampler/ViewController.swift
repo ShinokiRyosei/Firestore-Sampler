8f334b8f43f6374c01d23776272d0b1ae3209f2e
ShinokiRyosei <shinoki.r.0301@gmail.com>
2017-10-14 03:03:21 +0900

    setup tableview in ViewController

--- Firestore-Sampler/ViewController.swift
+++ Firestore-Sampler/ViewController.swift
@@ -8,10 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return 3
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") else {
+
+            return UITableViewCell()
+        }
+
+        return cell
     }
 }
 
