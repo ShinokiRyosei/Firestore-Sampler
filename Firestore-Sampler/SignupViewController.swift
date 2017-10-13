a8c0ef247a30c971354d1b7d0f6e23ca26adc002
ShinokiRyosei <shinoki.r.0301@gmail.com>
2017-10-14 03:04:27 +0900

    create SignupViewController

---
+++ Firestore-Sampler/SignupViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SignupViewController.swift
+//  Firestore-Sampler
+//
+//  Created by ShinokiRyosei on 2017/10/14.
+//  Copyright © 2017年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
