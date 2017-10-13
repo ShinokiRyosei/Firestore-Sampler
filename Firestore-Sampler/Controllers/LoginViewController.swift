6ffecf1d0f8485f9e425cdcd0483bd0f3e4fe411
ShinokiRyosei <shinoki.r.0301@gmail.com>
2017-10-14 03:59:10 +0900

    add login settings

--- Firestore-Sampler/Controllers/LoginViewController.swift
+++ Firestore-Sampler/Controllers/LoginViewController.swift
@@ -8,9 +8,44 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet var emailTextField: UITextField!
+
+    @IBOutlet var passwordTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+    }
+
+    @IBAction func didSelectedLogin(_ sender: UIButton) {
+
+        guard let email = emailTextField.text, let password = passwordTextField.text else {
+
+            return
+        }
+
+        if !email.isEmpty && !password.isEmpty {
+
+            self.login(withEmail: email, password: password)
+        }
+        else {
+
+            self.showErrorAlert(of: "メールアドレスとパスワードを入力してください")
+        }
+    }
+
+    func login(withEmail email: String, password: String) {
+
+    }
+
+    func showErrorAlert(of message: String) {
+
+        let alert = UIAlertController(title: "エラー", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
 }
