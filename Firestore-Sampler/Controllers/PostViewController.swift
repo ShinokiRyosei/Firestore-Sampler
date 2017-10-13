47f908e63271ba3bec7a6177925b6adef72dfa0d
ShinokiRyosei <shinoki.r.0301@gmail.com>
2017-10-14 04:18:32 +0900

    create PostViewController

---
+++ Firestore-Sampler/Controllers/PostViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PostViewController.swift
+//  Firestore-Sampler
+//
+//  Created by ShinokiRyosei on 2017/10/14.
+//  Copyright © 2017年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
