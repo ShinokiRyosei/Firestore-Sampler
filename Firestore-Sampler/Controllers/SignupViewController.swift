73fbb9971e0f05d11b97fb51c8b45a1137bccf86
ShinokiRyosei <shinoki.r.0301@gmail.com>
2017-10-14 03:22:51 +0900

    add signup settings

--- Firestore-Sampler/Controllers/SignupViewController.swift
+++ Firestore-Sampler/Controllers/SignupViewController.swift
@@ -8,9 +8,57 @@
 
 import UIKit
 
-class SignupViewController: UIViewController {
+class SignupViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet var emailTextField: UITextField!
+
+    @IBOutlet var passwordTextField: UITextField!
+
+    @IBOutlet var passwordConfirmTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        passwordTextField.delegate = self
+    }
+
+    @IBAction func didSelectedSignup(_ sender: UIButton) {
+
+        guard
+            let email = emailTextField.text,
+            let password = passwordTextField.text,
+            let passwordConfirm = passwordConfirmTextField.text else {
+
+            return
+        }
+
+        if !email.isEmpty && !password.isEmpty && !passwordConfirm.isEmpty {
+
+            if password == passwordConfirm {
+
+                self.signup(withEmail: email, password: password)
+            }
+            else {
+
+                self.showErrorAlert(of: "パスワードが一致しません")
+            }
+        }
+        else {
+
+            self.showErrorAlert(of: "メールアドレス・パスワードを入力してください")
+        }
+    }
+
+    func showErrorAlert(of message: String) {
+
+        let alert = UIAlertController(title: "エラー", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    func signup(withEmail email: String, password: String) {
+
     }
 }
