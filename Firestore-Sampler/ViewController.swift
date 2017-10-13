9f3f46a04bd06fc764df658f07b30b29d8ff02c8
ShinokiRyosei <shinoki.r.0301@gmail.com>
2017-10-14 02:46:00 +0900

    initial commit

---
+++ Firestore-Sampler/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Firestore-Sampler
+//
+//  Created by ShinokiRyosei on 2017/10/14.
+//  Copyright © 2017年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
